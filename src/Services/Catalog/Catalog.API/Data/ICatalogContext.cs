5ce313320c596a9a6705752bbf6da337d3aa062b
dev.one <95871431+dev.one@users.noreply.github.com>
2022-07-03 17:13:33 -0400

    -Develop Data Layer

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        //Stores product collection of MongoDB
+        IMongoCollection<Product> Products { get; }
+    }
+}
